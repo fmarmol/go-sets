8b7882e3d6ca2338606db3f96cad390594a3effd
florent <florent@packetai.co>
2021-12-19 21:58:25 +0100

    bump to go 1.18

---
+++ set_test.go
@@ -0,0 +1,36 @@
+package sets
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSetInt(t *testing.T) {
+
+	elems := []int{1, 2, 3, 4}
+
+	s := NewSet(elems...)
+	assert.True(t, s.Len() == len(elems))
+
+	for _, elem := range elems {
+		assert.True(t, s.Contains(elem))
+		s.Remove(elem)
+		assert.False(t, s.Contains(elem))
+	}
+	assert.True(t, s.Len() == 0)
+}
+
+func TestSetString(t *testing.T) {
+	elems := []string{"1", "2", "3", "4"}
+
+	s := NewSet(elems...)
+	assert.True(t, s.Len() == len(elems))
+
+	for _, elem := range elems {
+		assert.True(t, s.Contains(elem))
+		s.Remove(elem)
+		assert.False(t, s.Contains(elem))
+	}
+	assert.True(t, s.Len() == 0)
+}
